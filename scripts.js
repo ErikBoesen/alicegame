108d0043bf911d408d4452c83b1d0e1e5d00444d
ErikBoesen <me@erikboesen.com>
2021-07-05 09:34:04 -0400

    Add start of drawing loop and game mechanics

--- scripts.js
+++ scripts.js
@@ -3,4 +3,49 @@ canvas.height = HEIGHT * SIZE;
 canvas.width  = WIDTH * SIZE;
 let ctx = canvas.getContext('2d');
 
+const G = -9.8;
 
+class Player {
+    constructor() {
+        this.vel_x = 0;
+        this.vel_y = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+}
+let player = new Player();
+
+function tick() {
+    player.vel_y += G;
+}
+function draw() {
+    //ctx.fillStyle = 'rgb(0,' + (255/MAX_GRASS_GROWTH * grass[row][col]) + ',0)';
+    //ctx.fillRect(col * SIZE, row * SIZE, SIZE, SIZE);
+}
+function loop() {
+    tick();
+    draw();
+}
+
+canvas.onclick = function(e) {
+}
+
+window.onkeydown = function(e) {
+    let key = e.keyCode || e.which;
+
+    switch (key) {
+        // Up arrow
+        case 38:
+            player.vel_y = 5;
+            break;
+        // Down arrow
+        case 40:
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
+
+loop();
+let main_loop = setInterval(loop, 1);
