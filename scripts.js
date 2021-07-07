9b565557e6e845fcbd7bee59d579284159b57826
ErikBoesen <me@erikboesen.com>
2021-07-07 10:01:48 -0400

    Add some movement logic

--- scripts.js
+++ scripts.js
@@ -1,17 +1,28 @@
-let canvas = document.getElementById('canv');
-canvas.height = HEIGHT * SIZE;
-canvas.width  = WIDTH * SIZE;
+let canvas = document.getElementById('game');
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 let ctx = canvas.getContext('2d');
+
+ctx.msImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
+ctx.imageImageSmoothingEnabled = false;
+
+const SCALE = 5;
 
 const G = -9.8;
 
 const Orientation = {
-    LEFT: 0,
+    LEFT: -1,
     RIGHT: 1,
-}
+};
 
 class Player {
+    WALKING_SPEED = 5;
+
     constructor() {
+        this.size_x = 9;
+        this.size_y = 40;
         this.vel_x = 0;
         this.vel_y = 0;
         this.x = 0;
@@ -19,17 +30,32 @@ class Player {
         this.orientation = Orientation.RIGHT;
     }
 }
+
 let player = new Player();
 
+const image_names = [
+    'player',
+];
+const images = {};
+for (let image_name of image_names) {
+    let image = new Image();
+    image.src = 'images/' + image_name + '.png';
+    images[image_name] = image;
+}
+
 function tick() {
+    // Move player
     player.vel_y += G;
+    player.x += player.vel_x;
+    player.y += player.vel_y;
 }
 function draw() {
     //ctx.fillStyle = 'rgb(0,' + (255/MAX_GRASS_GROWTH * grass[row][col]) + ',0)';
     //ctx.fillRect(col * SIZE, row * SIZE, SIZE, SIZE);
 
     // Draw player
-
+    ctx.drawImage(images.player, SCALE * player.x, SCALE * player.y,
+                  SCALE * player.size_x, SCALE * player.size_y);
 }
 function loop() {
     tick();
@@ -49,6 +75,38 @@ window.onkeydown = function(e) {
             break;
         // Down arrow
         case 40:
+            break;
+        // Left arrow
+        case 37:
+            player.orientation = Orientation.LEFT;
+            player.vel_x = player.orientation * player.WALKING_SPEED;
+            break;
+        // Right arrow
+        case 39:
+            player.orientation = Orientation.RIGHT;
+            player.vel_x = player.orientation * player.WALKING_SPEED;
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
+window.onkeyup = function(e) {
+    let key = e.keyCode || e.which;
+
+    switch (key) {
+        // Up arrow
+        case 38:
+            player.vel_y = 5;
+            break;
+        // Down arrow
+        case 40:
+            break;
+        // Left arrow
+        case 37:
+        // Right arrow
+        case 39:
+            player.vel_x = 0;
             break;
         default:
             console.log(key);
