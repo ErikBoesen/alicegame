e0698e224ff3c6f476ca349110b46063050751d1
ErikBoesen <me@erikboesen.com>
2021-07-17 17:34:53 -0400

    Add new door in second room

--- scripts/map.js
+++ scripts/map.js
@@ -6,7 +6,9 @@ let rooms = [
     ], []),
     new Room('kitchen', 104, 7, 95, 55, false, true, true, [
         new Platform(0, 0, 95),
-    ], []),
+    ], [
+        new Door(93, 2, 2, Orientation.LEFT, false),
+    ]),
     new Room('storefronts', 199, 0, 508, 96, false, false, true, [
         new Platform(0, 5, 508),
         new Platform(23, 35, 62),
