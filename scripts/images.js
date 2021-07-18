9711067044dff58b9aa103a1ee93a3086889b494
ErikBoesen <me@erikboesen.com>
2021-07-18 18:20:58 -0400

    Remove images and looping

--- scripts/images.js
+++ scripts/images.js
@@ -1,12 +1,5 @@
-const imageNames = [
-    'door',
-];
 function getImage(imageName) {
     let image = new Image();
     image.src = 'images/' + imageName + '.png';
     return image;
-}
-const images = {};
-for (let imageName of imageNames) {
-    images[imageName] = getImage(imageName);
 }
