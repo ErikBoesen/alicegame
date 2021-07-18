2cc94e002cad0c99436ca9b234e8205b0eb8e974
ErikBoesen <me@erikboesen.com>
2021-07-18 12:36:58 -0400

    Add decoration model

---
+++ scripts/models/decoration.js
@@ -0,0 +1,11 @@
+class Decoration {
+    constructor(name, x, y, width, height, visible, update) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.visible = visible;
+        this.update = update;
+    }
+}
